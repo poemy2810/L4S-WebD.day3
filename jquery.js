f3579958a67796482984186340fef1440b6c5495
poemy_Okamoto <57334338+poemy2810@users.noreply.github.com>
2020-02-24 00:33:50 +0900

    変更した内容をメモ

--- jquery.js
+++ jquery.js
@@ -19,3 +19,13 @@ $('.slider').slick({
     fade:false,
     slidesToScroll:1
 });
+
+$('.main-visual').bgSwitcher({
+   images:['images/hero.jpg','images/background2.jpg','images/background3.jpg','images/background4.jpg','images/background5.jpg'], 
+　 interval: 6000,
+　 loop:true,
+　 shuffle:true,
+　 effect:"fade", // fade,blind,clip,slide,drop,hide
+　 duration:800,
+　 easing:"swing" // linear,swing
+   });
